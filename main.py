971ddec4f62c952dddda2c970426885b2798d589
olimarh <olimarh@simnet.is>
2024-10-25 13:30:13 +0000

    This should be the finished bookbot

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 word_count = 0
 char_count = {}
+book_path = "books/frankenstein.txt"
 def main():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
-        print(file_content)
+       
 
     words = file_content.split()
     word_count = len(words)
+    print(f"The book report of {book_path}")
     print(f"{word_count} words found in the document")
 
     lowered_file = file_content.lower()
     for char in lowered_file:
-        if char in char_count:
-            char_count[char] +=1
-        else:
-            char_count[char] = 1
-    print(char_count)
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] +=1
+            else:
+                char_count[char] = 1
+    
+    sorted_chars = sorted(char_count.items(), key=lambda item: item[1], reverse=True)
 
+    for char, count in sorted_chars:
+        print(f"The {char} character was found {count} times")       
 
 main()
